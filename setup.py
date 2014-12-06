8989a6b334e2959f5fcbbb0781b6b9596e7d42f1
John Gill <swfiua@gmail.com>
2014-12-06 14:05:01 -0400

    Initial version of eulasees.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import eulasees
+version = eulasees.__version__
+
+setup(
+    name='eulasees',
+    version=version,
+    author='Johnny Gill',
+    author_email='swfiua@gmail.com',
+    packages=[
+        'eulasees',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.7.1',
+    ],
+    zip_safe=False,
+    scripts=['eulasees/manage.py'],
+)
